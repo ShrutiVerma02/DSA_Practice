661be1f10a0a8e6b3128351fd7fc47be36586ef8
ShrutiVerma02 <88986886+ShrutiVerma02@users.noreply.github.com>
2022-01-28 23:10:45 +0530

    Create sum.java

---
+++ Basic/sum.java
@@ -0,0 +1,14 @@
+public class FirstJavaProgram {
+
+	public static void main(String[] args) {
+		
+		int num1 = 5;
+		int num2 = 8;
+		int num3 = 12;
+		int sum = num1 + num2 + num3;
+		
+		System.out.println(sum);
+
+	}
+
+}
